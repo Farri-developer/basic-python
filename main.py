3752898e18d501006a384045666c3db9b3ce9286
Farhan Ayub <fayub511@gmail.com>
2025-11-30 00:25:56 +0500

    Basic Flask API code using local network and localhost to print 'Hello Flask API and Farhan at port Number 5000'

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from flask import Flask, jsonify
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({
+        "message": "Hello Flask API"
+    })
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/name", methods=["GET"])
+def get_name():
+    return jsonify({
+        "name": "Farhan"
+    })
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
